a6314d9f1b622c21868c17e9c7d8fb1a4a0d8968
paulsadofsky <74940533+paulsadofsky@users.noreply.github.com>
2025-03-18 14:53:48 -0400

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -5,17 +5,73 @@ import random
 # numbers = 48-57
 # special characters = 33, 35, 36, 37, 38, 42, 60, 62, 63, 64, 94, 95
 
-excluded = [34, 39, 40, 41, 43, 44, 45, 46, 47, 58, 59, 61, 91, 92, 93, 96]
-upper_bound = 122
-lower_bound = 33
-password = ""
-password_length = 10
+def main():
+    password_length = 10
+    password = ""
 
-for i in range(password_length):
-    rand_char = random.randint(lower_bound, upper_bound)
-    while (rand_char in excluded):
-        rand_char = random.randint(lower_bound, upper_bound)
-    password += chr(rand_char)
+    userinput = ""
+    while (userinput != "5"):
+        print("MENU OPTIONS:\n",
+              "1 - Lowercase\n",
+              "2 - Lowercase + Uppercase\n",
+              "3 - Lowercase + Uppercase + Numbers\n",
+              "4 - Lowercase + Uppercase + Numbers + Special\n",
+              "5 - Exit\n")
+        userinput = input("Enter an option: ")
+    
+        if (userinput == '1'):
+            upper_bound = 122
+            lower_bound = 97
+            password = ""
 
-print("PASSWORD: " + password)
+            for i in range(password_length):
+                rand_char = random.randint(lower_bound, upper_bound)
+                password += chr(rand_char)
 
+        elif (userinput == '2'):
+            upper_bound = 122
+            lower_bound = 65
+            password = ""
+
+            for i in range(password_length):
+                rand_char = random.randint(lower_bound, upper_bound)
+                while (rand_char <= 96 and rand_char >= 91):
+                    rand_char = random.randint(lower_bound, upper_bound)
+                password += chr(rand_char)
+        
+        elif (userinput == '3'):
+            upper_bound = 122
+            lower_bound = 48
+            password = ""
+
+            for i in range(password_length):
+                rand_char = random.randint(lower_bound, upper_bound)
+                while ((rand_char <= 96 and rand_char >= 91) or (rand_char <= 64 and rand_char >= 58)):
+                    rand_char = random.randint(lower_bound, upper_bound)
+                password += chr(rand_char)
+
+        elif (userinput == '4'):
+            excluded = [34, 39, 44, 46, 47, 58, 59, 92, 96]
+            upper_bound = 122
+            lower_bound = 33
+            password = ""
+            password_length = 10
+
+            for i in range(password_length):
+                rand_char = random.randint(lower_bound, upper_bound)
+                while (rand_char in excluded):
+                    rand_char = random.randint(lower_bound, upper_bound)
+                password += chr(rand_char)
+
+        elif (userinput == '5'):
+            break
+
+        else:
+            print("INVALID INPUT\n")
+            continue
+
+        print("PASSWORD: " + password, "\n")
+
+    return
+
+main()
