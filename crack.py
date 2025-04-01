b8d7f87c1f1e18721088df72d12836827bb79c44
Tiago Alfonso Wells <47674498+tigtaw@users.noreply.github.com>
2025-04-01 14:00:04 -0400

    Untested cracking

---
+++ crack.py
@@ -0,0 +1,21 @@
+def validate_password(str):
+    return password == str
+
+def crack_password(valid_chars: str):
+    min = 1
+    max = 20
+    password_iter = password_gen(valid_chars, min, max)
+    for password in password_iter:
+        if validate_password(password):
+            return password
+    return None
+
+def password_gen(valid_chars: str, min: int, max: int) => str:
+    password = "".join([valid_chars[0] for _ in range(min)])
+    yield password
+    for i in range(min, max + 1):
+        for j in range(len(password)):
+            for k in valid_chars:
+                yield password := f"{password[:j]}{k}{password[j+1:]}"
+        if (len(password) < max):
+        password = f"{valid_chars[0]}{password}"
