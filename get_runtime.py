2d234be4070f270249beb8e97a3285cb67e967ec
paulsadofsky <74940533+paulsadofsky@users.noreply.github.com>
2025-04-15 22:18:29 -0400

    Add files via upload

--- get_runtime.py
+++ get_runtime.py
@@ -4,6 +4,7 @@ import random, threading, time
 def generate_password(chars: str, length):
     password = ''
     for i in range(length):
+        # Generates a random character from list of random characters based off of length
         rand_char = chars[random.randint(0, len(chars)-1)]
         password += rand_char
 
@@ -14,14 +15,20 @@ def crack_password(actual_password: str, valid_chars: str, length: int) -> Optio
     len_valid = len(valid_chars)
     guess_indices = []
     target_indices = []
+
+    # Breaks the actual password into a list of indices from the valid_chars string
+    # Uses this to generate "guess_indices" until all indices match
     for i in range(length):
         guess_indices.append(0)
         target_indices.append(valid_chars.index(actual_password[i]))
 
     start_time = time.time()
+    # Loop until guess is correct
     while(guess_indices != target_indices):
+        # Increment counter for number of guess the attack has taken
         iterations += 1
 
+        # If the lowest indices has reached its maximum value, it should reset to 0 and the next index should increment
         index = 0
         while index < length:
             guess_indices[index] += 1
@@ -33,66 +40,45 @@ def crack_password(actual_password: str, valid_chars: str, length: int) -> Optio
     end_time = time.time()
     execution_time = end_time - start_time
     
+    # Exiting the while loop signifies that the password has been cracked
     return iterations, execution_time
-
-from typing import Optional
-
-
-def get_valid_chars(case):
-    valchar = "abcdefghijklmnopqrstuvwxyz"
-
-    if (case >= 2):
-        valchar += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    
-    if (case >= 3):
-        valchar += "0123456789"
-
-    if (case >= 4):
-        valchar += "!@#$%^&*()[]+=.,<>-_"
-
-    return valchar
-
-def run_test(length, case):
-    valid = get_valid_chars(case)
-    password = generate_password(valid, length)
-    count, time = crack_password(password, valid, length)
-
-    return count, time
-
-def run_loop(length, case, loops):
-    average_iterations = 0
-    average_time = 0
-    for i in range(loops):
-        run_count, run_time = run_test(length, case)
-        average_iterations += run_count
-        average_time += run_time
-    average_iterations = int(average_iterations/loops)
-    average_time = average_time/loops
-
-    return average_iterations, average_time
 
 #########################################################################
 #                             MAIN FUNCTION                             #
 #########################################################################
 
-# Password Cases:
-# 1 - Lowercase
-# 2 - Lowercase + Uppercase
-# 3 - Lowercase + Uppercase + Numbers
-# 4 - Lowercase + Uppercase + Numbers + Special
-
 if __name__ == '__main__':
     print("Running simulations and calculations...")
-    avgitr, avgtime = run_loop(length=3, case=4, loops=100)
+
+    # Characters that make up the passwords for case 4
+    valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()[]+=.,<>-_"
+    
+    length = 3
+    case = 4
+    loops = 100
+    
+    # Runs the generation and cracking function at a user-defined number of times, then averages the guesses and time taken to complete
+    average_iterations = 0
+    average_time = 0
+    for i in range(loops):
+        password = generate_password(valid, length)
+        run_count, run_time = crack_password(password, valid, length)
+        average_iterations += run_count
+        average_time += run_time
+    avgitr = int(average_iterations/loops)
+    avgtime = average_time/loops
+
+    # Calculates the rate of the system's performance in terms of seconds per password guess generation and validation
     seconds_per_guess = avgtime/avgitr
 
     rows, cols = 4, 10
     computation_time = [[0 for j in range(cols)] for i in range(rows)]
 
+    # Sorts password by proper order of magnitude for better intuitive interpretations of the data
     for i in range(rows):
         print()
         for j in range(cols):
-            num_combonations = pow(len(get_valid_chars(i+1)),j+1)
+            num_combonations = pow(len(valid),j+1)
             avg_time_for_sim = ((num_combonations*seconds_per_guess)/2)*1000000
             print_statement = "Average time to crack a single case " + str(i+1) + " password at length " + str(j+1) + ":\t"
 
--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 from typing import Optional
 import random, threading, time
 
+# Initializes arrays to store simulation results into
 rows, cols = 4, 20
 results = [[0 for i in range(cols)] for j in range(rows)]
 results_time = [[0 for i in range(cols)] for j in range(rows)]
@@ -8,6 +9,7 @@ results_time = [[0 for i in range(cols)] for j in range(rows)]
 def generate_password(chars: str, length):
     password = ''
     for i in range(length):
+        # Generates a random character from list of random characters based off of length
         rand_char = chars[random.randint(0, len(chars)-1)]
         password += rand_char
 
@@ -18,14 +20,20 @@ def crack_password(actual_password: str, valid_chars: str, length: int) -> Optio
     len_valid = len(valid_chars)
     guess_indices = []
     target_indices = []
+
+    # Breaks the actual password into a list of indices from the valid_chars string
+    # Uses this to generate "guess_indices" until all indices match
     for i in range(length):
         guess_indices.append(0)
         target_indices.append(valid_chars.index(actual_password[i]))
 
     start_time = time.time()
+    # Loop until guess is correct
     while(guess_indices != target_indices):
+        # Increment counter for number of guess the attack has taken
         iterations += 1
 
+        # If the lowest indices has reached its maximum value, it should reset to 0 and the next index should increment
         index = 0
         while index < length:
             guess_indices[index] += 1
@@ -37,25 +45,29 @@ def crack_password(actual_password: str, valid_chars: str, length: int) -> Optio
     end_time = time.time()
     execution_time = end_time - start_time
     
+    # Exiting the while loop signifies that the password has been cracked
     return iterations, execution_time
 
-from typing import Optional
-
-
+# Returns the characters that make up the passwords at each case
 def get_valid_chars(case):
+    # Base Case (1)
     valchar = "abcdefghijklmnopqrstuvwxyz"
 
     if (case >= 2):
+        # Case 2
         valchar += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     
     if (case >= 3):
+        # Case 3
         valchar += "0123456789"
 
     if (case >= 4):
+        # Case 4
         valchar += "!@#$%^&*()[]+=.,<>-_"
 
     return valchar
 
+# Wrapper function to run a single password generation and crack
 def run_test(length, case):
     valid = get_valid_chars(case)
     password = generate_password(valid, length)
@@ -63,6 +75,7 @@ def run_test(length, case):
 
     return count, time
 
+# Runs the generation and cracking function at a user-defined number of times, then averages the guesses and time taken to complete
 def run_loop(length, case, loops, min_length):
     average_iterations = 0
     average_time = 0
@@ -78,12 +91,14 @@ def run_loop(length, case, loops, min_length):
 
     return
 
+# Runs loop function for a specified password case from a minimum length to a maximum length
 def run_case(min_length, max_length, case, loops):
     for i in range(max_length-min_length+1):
         run_loop(min_length+i, case, loops, min_length)
     
     return
 
+# Multi-threds teh execution of the run_case function across the four cases to decrease simulation runtime
 def run_sim(min, max, loops):
     threads = []
     for i in range(4):
@@ -94,6 +109,7 @@ def run_sim(min, max, loops):
     for thread in threads:
         thread.join()
 
+    # Print results to terminal
     for i in range(4):
         print("")
         print("------------------------------ CASE", i+1, "------------------------------")
@@ -113,8 +129,10 @@ def run_sim(min, max, loops):
 # 4 - Lowercase + Uppercase + Numbers + Special
 
 if __name__ == '__main__':
-    min = 2
-    max = 4
-    loops = 1
+    min = 1
+    max = 3
+    loops = 100
 
+    # Calculates the average number of guesses and time taken to crack a password over a specified number of runs between a minimum and maximum password length
+    # Results are outputted to the terminal
     run_sim(min, max, loops)
